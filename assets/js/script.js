9460891ffc1ba53a4c8e810daadb5b49ff8a8ac4
beamchristian <beamchristian@yahoo.com>
2021-09-30 19:01:53 -0400

    fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,59 @@
-const getGenre = document.querySelector('#find-genre');
-const getPlaylist = document.querySelector('#get-playlist');
+//     'https://api.nasa.gov/planetary/apod?api_key=nndUMOcl1g9pH4trtcU5C4Reha8gBSZ4cOfJrlWU'
 
-getGenre.addEventListener('click', function () {
-  console.log('clicked getGenre');
-});
+// Api for NASA APOD https://api.nasa.gov/planetary/apod?api_key=nndUMOcl1g9pH4trtcU5C4Reha8gBSZ4cOfJrlWU
 
-getPlaylist.addEventListener('click', function () {
-  console.log('clicked getPlaylist');
-});
+const getLaunches = document.querySelector('#launchBtn');
+const getImages = document.querySelector('#imageBtn');
+
+const getLaunchesApi = function (user) {
+  // format the github api url
+  const apiUrlLaunches = 'https://lldev.thespacedevs.com/2.2.0/';
+
+  // make a request to the url
+  fetch(apiUrlLaunches)
+    .then(function (response) {
+      // request was succesful
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    })
+    .catch(function (error) {
+      // .catch gets chained onto the end of the `.then()` method.
+      alert('Unable to connect to GitHub');
+    });
+};
+
+const getImagesApi = function (user) {
+  // format the github api url
+  const apiUrlImages =
+    'https://api.nasa.gov/planetary/apod?api_key=nndUMOcl1g9pH4trtcU5C4Reha8gBSZ4cOfJrlWU';
+
+  // make a request to the url
+  fetch(apiUrlImages)
+    .then(function (response) {
+      // request was succesful
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    })
+    .catch(function (error) {
+      // .catch gets chained onto the end of the `.then()` method.
+      alert('Unable to connect to GitHub');
+    });
+};
+
+//     'https://api.nasa.gov/planetary/apod?api_key=nndUMOcl1g9pH4trtcU5C4Reha8gBSZ4cOfJrlWU'
+
+// Api for NASA APOD https://api.nasa.gov/planetary/apod?api_key=nndUMOcl1g9pH4trtcU5C4Reha8gBSZ4cOfJrlWU
+
+getLaunches.addEventListener('click', getLaunchesApi);
+
+getImages.addEventListener('click', getImagesApi);
