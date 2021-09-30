f81fbc4a45f34ebbabfe592719c65ba19b38f237
beamchristian <beamchristian@yahoo.com>
2021-09-29 20:36:25 -0400

    query selectors and click events

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const getGenre = document.querySelector('#find-genre');
+const getPlaylist = document.querySelector('#get-playlist');
+
+getGenre.addEventListener('click', function () {
+  console.log('clicked getGenre');
+});
+
+getPlaylist.addEventListener('click', function () {
+  console.log('clicked getPlaylist');
+});
